6ac8dbd7ee4854656eec3a80de5fb6360e51d363
Anton Sidorov <anton@ubernil.com>
2019-02-13 16:02:07 +0300

    mix: update config for publishing

--- mix.exs
+++ mix.exs
@@ -5,20 +5,29 @@ defmodule Jiffex.Mixfile do
     [
       app: :jiffex,
       version: "0.2.0",
+      name: "Jiffex",
+      description: "A simple wrap over :jiffy library to use it in Elixir projects",
       elixir: "~> 1.6",
       start_permanent: Mix.env == :prod,
       deps: deps(),
-      package: package()
+      package: package(),
+      source: "https://github.com/KosyanMedia/jiffex"
     ]
   end
 
   def application, do: [extra_applications: []]
 
-  defp deps, do: [{:jiffy, "~> 0.15"}]
+  defp deps do
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:jiffy, "~> 0.15"}
+    ]
+  end
 
   defp package do
     [
-      files: ["lib", "mix.exs", "README.md"],
+      name: "jiffex",
+      licenses: ["BSD License"],
       maintainers: ["Anton Sidorov"],
       links: %{"GitHub" => "https://github.com/KosyanMedia/jiffex"}
     ]
